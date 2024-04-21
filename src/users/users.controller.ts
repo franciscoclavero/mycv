5f6c9ec0132cdabad5f18c018e16a7db905788df
franciscoclavero <franciscogkoclavero@gmail.com>
2024-04-20 22:16:32 -0300

    feat: Refactor UsersController to include CRUD operations for user management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Query,
+  Post,
+  Get,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,5 +20,27 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const user = this.usersService.create(body.email, body.password);
     return user;
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    const user = this.usersService.findOne(parseInt(id));
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    const users = this.usersService.find(email);
+    return users;
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
