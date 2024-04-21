10eb396a21fe43f11427b864c99e4a71f23ef035
franciscoclavero <franciscogkoclavero@gmail.com>
2024-04-21 15:56:44 -0300

    feat: Refactor UsersController to include UserDto in SerializeInterceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -24,10 +25,9 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   findUser(@Param('id') id: string) {
-    console.log('Between serialization');
     const user = this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('User not found');
