5ebaa0f9c16a6f4e7a66b2a99ae3dc4cba992c10
franciscoclavero <franciscogkoclavero@gmail.com>
2024-04-21 16:06:53 -0300

    feat: Refactor UsersController to use @Serialize decorator instead of SerializeInterceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,13 +8,12 @@ import {
   Patch,
   Delete,
   NotFoundException,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -25,7 +24,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     const user = this.usersService.findOne(parseInt(id));
