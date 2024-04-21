f1525925530c76e8a2c1dd7faf8e70fd5b96f493
franciscoclavero <franciscogkoclavero@gmail.com>
2024-04-21 16:20:15 -0300

    feat: Refactor SerializeInterceptor to use ClassConstructor interface in serialize.interceptor.ts

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,20 +7,21 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
+import { ClassConstructor } from '../types/ClassConstructor.type';
 
-export function Serialize(dto: any) {
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  constructor(private dto: ClassConstructor) {}
 
   intercept(
     context: ExecutionContext,
-    next: CallHandler<any>,
+    next: CallHandler<ClassConstructor>,
   ): Observable<any> {
     return next.handle().pipe(
-      map((data) => {
+      map((data: ClassConstructor) => {
         return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
